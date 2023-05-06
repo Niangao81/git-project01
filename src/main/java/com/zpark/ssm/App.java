21d67684fb05d6bc315bd59179bb7c0e4e613942
gyt <gyt@163.com>
2023-05-06 09:32:21 +0800

    gyt master first modify!

--- src/main/java/com/zpark/ssm/App.java
+++ src/main/java/com/zpark/ssm/App.java
@@ -4,8 +4,13 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+
         System.out.println("Hello Git! gyt master second commit!");
+
         System.out.println("Hello Git! gyt hotfix first commit!");
+
+        System.out.println("Hello Git! gyt master first modify!");
+
         System.out.println("Hello Git! gyt hotfix first modify!");
     }
 
